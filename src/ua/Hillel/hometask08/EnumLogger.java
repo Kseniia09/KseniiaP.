b3be7a78379b22cdd7008cb76d6d241f94601a5e
House <p.kseniia09@gmail.com>
2023-05-10 16:14:43 +0300

    Test

--- src/ua/Hillel/hometask08/EnumLogger.java
+++ src/ua/Hillel/hometask08/EnumLogger.java
@@ -1,11 +1,11 @@
 package ua.Hillel.hometask08;
 
 public class EnumLogger {
-    private static final String ANSI_RESET = "\u001B[0m";
-    private static final String BLACK_BRIGHT = "\033[0;90m";//info
-    private static final String RED_BRIGHT = "\033[0;91m";//error
-    private static final String GREEN_BRIGHT = "\033[0;92m";//warn
-    private static final String YELLOW_BRIGHT = "\033[0;93m";//debug
+    private static final String AR = "\u001B[0m";
+    private static final String BR = "\033[0;90m";//info
+    private static final String RB = "\033[0;91m";//error
+    private static final String GB = "\033[0;92m";//warn
+    private static final String YB = "\033[0;93m";//debug
 
     public enum LogLevel {
         INFO,
@@ -15,10 +15,10 @@ public class EnumLogger {
     }
     public void log(LogLevel level, String message){
         switch (level){
-            case INFO -> System.out.println(BLACK_BRIGHT + "[INFO]:" + ANSI_RESET + message);
-            case WARN -> System.out.println(GREEN_BRIGHT + "[WARN]:" + ANSI_RESET + message);
-            case DEBUG -> System.out.println(YELLOW_BRIGHT + "[DEBUG]:" + ANSI_RESET + message);
-            case ERROR -> System.out.println(RED_BRIGHT + "[ERROR]" + ANSI_RESET + message);
+            case INFO -> System.out.println(BR + "[INFO]:" + AR + message);
+            case WARN -> System.out.println(GB + "[WARN]:" + AR+ message);
+            case DEBUG -> System.out.println(YB + "[DEBUG]:" + AR + message);
+            case ERROR -> System.out.println(RB + "[ERROR]" + AR + message);
         }
     }
 }
